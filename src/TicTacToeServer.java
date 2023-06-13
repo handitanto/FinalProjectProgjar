2f8554447cdad49670dad4bc631148bff96d4912
kielmw <75736236+kielmw@users.noreply.github.com>
2023-06-13 22:07:36 +0700

    Revert "+ Room - Chat"
    This reverts commit 3be9bcdb335879058dde0e090bb17f786e5aea04.

--- src/TicTacToeServer.java
+++ src/TicTacToeServer.java
@@ -11,10 +11,18 @@ public class TicTacToeServer {
     private static final int PORT = 12345;
 
     private ServerSocket serverSocket;
-    private List<Room> rooms;
+    private List<Socket> playerSockets;
+    private List<PrintWriter> playerWriters;
+    private List<BufferedReader> playerReaders;
+    private char[] board;
+    private int currentPlayer;
 
     public TicTacToeServer() {
-        rooms = new ArrayList<>();
+        playerSockets = new ArrayList<>();
+        playerWriters = new ArrayList<>();
+        playerReaders = new ArrayList<>();
+        board = new char[9];
+        currentPlayer = 0;
     }
 
     public void startServer() {
@@ -22,203 +30,147 @@ public class TicTacToeServer {
             serverSocket = new ServerSocket(PORT);
             System.out.println("Server started. Waiting for players to join...");
 
-            while (true) {
-                Socket playerSocket1 = serverSocket.accept();
-                Socket playerSocket2 = serverSocket.accept();
-                Room room = new Room(playerSocket1, playerSocket2);
-                rooms.add(room);
-                room.start();
+            for (int i = 0; i < 2; i++) {
+                Socket playerSocket = serverSocket.accept();
+                playerSockets.add(playerSocket);
+                playerWriters.add(new PrintWriter(playerSocket.getOutputStream(), true));
+                playerReaders.add(new BufferedReader(new InputStreamReader(playerSocket.getInputStream())));
+
+                playerWriters.get(i).println("Welcome, Player " + (i + 1) + "! You are Player " + (i + 1) + ".");
             }
+
+            System.out.println("Both players have joined. Starting the game.");
+
+            initializeBoard();
+
+            // Main game loop
+            while (true) {
+                int currentPlayerIndex = currentPlayer % 2;
+                int opponentIndex = (currentPlayer + 1) % 2;
+
+                // Send board state to the current player
+                playerWriters.get(currentPlayerIndex).println(getBoardState());
+
+                // Prompt the current player to make a move
+                playerWriters.get(currentPlayerIndex).println("Your move (row,column):");
+                String move = playerReaders.get(currentPlayerIndex).readLine();
+
+                if (move.equals("quit")) {
+                    playerWriters.get(currentPlayerIndex).println("You have quit the game.");
+                    playerWriters.get(opponentIndex).println("Your opponent has quit the game.");
+                    break;
+                }
+
+                if (isValidMove(move)) {
+                    updateBoard(move, currentPlayerIndex);
+                    playerWriters.get(currentPlayerIndex).println("MOVE," + move);
+                    playerWriters.get(opponentIndex).println("MOVE," + move);
+
+                    if (checkWin(currentPlayerIndex)) {
+                        playerWriters.get(currentPlayerIndex).println("Congratulations! You win!");
+                        playerWriters.get(opponentIndex).println("Game over. You lose.");
+                        break;
+                    }
+
+                    if (isBoardFull()) {
+                        playerWriters.get(currentPlayerIndex).println("Game over. It's a draw.");
+                        playerWriters.get(opponentIndex).println("Game over. It's a draw.");
+                        break;
+                    }
+
+                    currentPlayer++;
+                } else if (move.startsWith("t")) {
+                    String chatMessage = move.substring(2);
+                    for (PrintWriter writer : playerWriters) {
+                        writer.println("CHAT: Player " + (currentPlayerIndex + 1) + ": " + chatMessage);
+                    }
+                } else {
+                    playerWriters.get(currentPlayerIndex).println("INVALID_MOVE");
+                }
+            }
+
+            serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private class Room extends Thread {
-        private Socket playerSocket1;
-        private Socket playerSocket2;
-        private PrintWriter writer1;
-        private PrintWriter writer2;
-        private BufferedReader reader1;
-        private BufferedReader reader2;
-        private char[] board;
-        private int currentPlayer;
-
-        public Room(Socket playerSocket1, Socket playerSocket2) {
-            this.playerSocket1 = playerSocket1;
-            this.playerSocket2 = playerSocket2;
-            currentPlayer = 0;
-            board = new char[9];
+    private void initializeBoard() {
+        for (int i = 0; i < 9; i++) {
+            board[i] = '-';
         }
+    }
 
-        public void run() {
-            try {
-                writer1 = new PrintWriter(playerSocket1.getOutputStream(), true);
-                writer2 = new PrintWriter(playerSocket2.getOutputStream(), true);
-                reader1 = new BufferedReader(new InputStreamReader(playerSocket1.getInputStream()));
-                reader2 = new BufferedReader(new InputStreamReader(playerSocket2.getInputStream()));
+    private String getBoardState() {
+        StringBuilder boardState = new StringBuilder();
 
-                writer1.println("Welcome, Player 1! You are Player 1.");
-                writer2.println("Welcome, Player 2! You are Player 2.");
-
-                initializeBoard();
-
-                // Main game loop
-                while (true) {
-                    sendBoardState(0);
-                    sendPrompt(0);
-                    String move = reader1.readLine();
-
-                    if (move.equals("quit")) {
-                        writer1.println("You have quit the game.");
-                        writer2.println("Your opponent has quit the game.");
-                        break;
-                    }
-
-                    if (isValidMove(move)) {
-                        updateBoard(move, 0);
-                        sendMoveToPlayers(move);
-
-                        if (checkWin(0)) {
-                            writer1.println("Congratulations! You win!");
-                            writer2.println("Game over. You lose.");
-                            break;
-                        }
-
-                        if (isBoardFull()) {
-                            writer1.println("Game over. It's a draw.");
-                            writer2.println("Game over. It's a draw.");
-                            break;
-                        }
-
-                        sendBoardState(1);
-                        sendPrompt(1);
-                        move = reader2.readLine();
-
-                        if (move.equals("quit")) {
-                            writer2.println("You have quit the game.");
-                            writer1.println("Your opponent has quit the game.");
-                            break;
-                        }
-
-                        if (isValidMove(move)) {
-                            updateBoard(move, 1);
-                            sendMoveToPlayers(move);
-
-                            if (checkWin(1)) {
-                                writer2.println("Congratulations! You win!");
-                                writer1.println("Game over. You lose.");
-                                break;
-                            }
-
-                            if (isBoardFull()) {
-                                writer2.println("Game over. It's a draw.");
-                                writer1.println("Game over. It's a draw.");
-                                break;
-                            }
-                        } else {
-                            writer2.println("INVALID_MOVE");
-                        }
-                    } else {
-                        writer1.println("INVALID_MOVE");
-                    }
-                }
-
-                playerSocket1.close();
-                playerSocket2.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+        for (int i = 0; i < 9; i++) {
+            boardState.append(board[i]);
+            if ((i + 1) % 3 == 0) {
+                boardState.append("\n");
             }
         }
 
-        private void initializeBoard() {
-            for (int i = 0; i < 9; i++) {
-                board[i] = '-';
-            }
-        }
+        return boardState.toString();
+    }
 
-        private void sendBoardState(int playerIndex) {
-            PrintWriter writer = (playerIndex == 0) ? writer1 : writer2;
-
-            StringBuilder boardState = new StringBuilder();
-            for (int i = 0; i < 9; i++) {
-                boardState.append(board[i]);
-                if ((i + 1) % 3 == 0) {
-                    boardState.append("\n");
-                }
-            }
-
-            writer.println(boardState.toString());
-        }
-
-        private void sendPrompt(int playerIndex) {
-            PrintWriter writer = (playerIndex == 0) ? writer1 : writer2;
-            writer.println("Your move (row,column):");
-        }
-
-        private boolean isValidMove(String move) {
-            if (move.length() != 3) {
-                return false;
-            }
-
-            int row = Character.getNumericValue(move.charAt(0));
-            int col = Character.getNumericValue(move.charAt(2));
-
-            if (row < 0 || row > 2 || col < 0 || col > 2) {
-                return false;
-            }
-
-            int position = row * 3 + col;
-            return board[position] == '-';
-        }
-
-        private void updateBoard(String move, int playerIndex) {
-            int row = Character.getNumericValue(move.charAt(0));
-            int col = Character.getNumericValue(move.charAt(2));
-
-            int position = row * 3 + col;
-            board[position] = (playerIndex == 0) ? 'X' : 'O';
-        }
-
-        private void sendMoveToPlayers(String move) {
-            writer1.println("MOVE," + move);
-            writer2.println("MOVE," + move);
-        }
-
-        private boolean checkWin(int playerIndex) {
-            char symbol = (playerIndex == 0) ? 'X' : 'O';
-
-            // Check rows
-            for (int i = 0; i < 3; i++) {
-                if (board[i * 3] == symbol && board[i * 3 + 1] == symbol && board[i * 3 + 2] == symbol) {
-                    return true;
-                }
-            }
-
-            // Check columns
-            for (int i = 0; i < 3; i++) {
-                if (board[i] == symbol && board[i + 3] == symbol && board[i + 6] == symbol) {
-                    return true;
-                }
-            }
-
-            // Check diagonals
-            if ((board[0] == symbol && board[4] == symbol && board[8] == symbol)
-                    || (board[2] == symbol && board[4] == symbol && board[6] == symbol)) {
-                return true;
-            }
-
+    private boolean isValidMove(String move) {
+        if (move.length() != 3) {
             return false;
         }
 
-        private boolean isBoardFull() {
-            for (int i = 0; i < 9; i++) {
-                if (board[i] == '-') {
-                    return false;
-                }
+        int row = Character.getNumericValue(move.charAt(0));
+        int col = Character.getNumericValue(move.charAt(2));
+
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+
+        int position = row * 3 + col;
+        return board[position] == '-';
+    }
+
+    private void updateBoard(String move, int playerIndex) {
+        int row = Character.getNumericValue(move.charAt(0));
+        int col = Character.getNumericValue(move.charAt(2));
+
+        int position = row * 3 + col;
+        board[position] = (playerIndex == 0) ? 'X' : 'O';
+    }
+
+    private boolean checkWin(int playerIndex) {
+        char symbol = (playerIndex == 0) ? 'X' : 'O';
+
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i * 3] == symbol && board[i * 3 + 1] == symbol && board[i * 3 + 2] == symbol) {
+                return true;
             }
+        }
+
+        // Check columns
+        for (int i = 0; i < 3; i++) {
+            if (board[i] == symbol && board[i + 3] == symbol && board[i + 6] == symbol) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if ((board[0] == symbol && board[4] == symbol && board[8] == symbol)
+                || (board[2] == symbol && board[4] == symbol && board[6] == symbol)) {
             return true;
         }
+
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 9; i++) {
+            if (board[i] == '-') {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
